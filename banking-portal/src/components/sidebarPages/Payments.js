998c6e6705aacd639e337895b8b5b8225e3f1c67
Quang <tangquang411@gmail.com>
2020-11-21 12:22:37 -0800

    add ui payment and transfer

--- banking-portal/src/components/sidebarPages/Payments.js
+++ banking-portal/src/components/sidebarPages/Payments.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Container, Row, Col, Card } from "react-bootstrap"
+import {Button, InputGroup, DropdownButton, Dropdown, FormControl, Container, Row, Col, Card } from "react-bootstrap"
 import Sidebar from "../dashboardcomponents/Sidebar"
 import "../../styles/Dashboard.css"
 
@@ -14,6 +14,27 @@ export default function Payments () {
                 <Col>
                     <Card className="header">
                         <h2>Payments</h2>
+                        <InputGroup className="mb-3">
+                            <InputGroup.Prepend>
+                                <DropdownButton
+                                    as={InputGroup.Prepend}
+                                    variant="outline-secondary"
+                                    title="Dropdown"
+                                    id="input-group-dropdown-1"
+                                >
+
+                                <Dropdown.Item href="#">Paybill</Dropdown.Item>
+                                <Dropdown.Item href="#">Credit card</Dropdown.Item>
+
+                                </DropdownButton>
+                            <FormControl
+                                    placeholder="Sender's username"
+                                    aria-label="Amount (to the nearest dollar)"/>
+                                <InputGroup.Text>$</InputGroup.Text>
+                                <input type="text" pattern="[0-9]*" placeholder = "0.00"/>
+                            </InputGroup.Prepend>
+                      </InputGroup>
+                      <Button variant="primary">Submit</Button>{' '}
                     </Card>
                 </Col>
             </Row>
--- banking-portal/src/components/sidebarPages/Transfers.js
+++ banking-portal/src/components/sidebarPages/Transfers.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Container, Row, Col, Card } from "react-bootstrap"
+import {Button, Dropdown, DropdownButton, InputGroup, FormControl, Container, Row, Col, Card } from "react-bootstrap"
 import Sidebar from "../dashboardcomponents/Sidebar"
 import "../../styles/Dashboard.css"
 
@@ -14,6 +14,52 @@ export default function Transfers () {
                 <Col>
                     <Card classname="header">
                         <h2>Transfers</h2>
+                        <div>
+                <InputGroup className="mb-3">
+                    <h4>From: </h4>
+                    <InputGroup.Prepend>
+                        <DropdownButton
+                            as={InputGroup.Prepend}
+                            variant="outline-secondary"
+                            title="Dropdown"
+                            id="input-group-dropdown-1"
+                        >
+
+                        <Dropdown.Item href="#">Checking Account</Dropdown.Item>
+                        <Dropdown.Item href="#">Saving Account</Dropdown.Item>
+
+                        </DropdownButton>
+                    <FormControl
+                            placeholder="Sender's username"
+                            aria-label="Amount (to the nearest dollar)"/>
+                        <InputGroup.Text>$</InputGroup.Text>
+                        <input type="text" pattern="[0-9]*" placeholder = "0.00"/>
+                    </InputGroup.Prepend>
+              </InputGroup>
+      <InputGroup className="mb-3">
+          <h4>To: </h4>
+          <InputGroup.Prepend>
+              <DropdownButton
+                  as={InputGroup.Prepend}
+                  variant="outline-secondary"
+                  title="Dropdown"
+                  id="input-group-dropdown-1"
+              >
+
+              <Dropdown.Item href="#">Checking Account</Dropdown.Item>
+              <Dropdown.Item href="#">Saving Account</Dropdown.Item>
+
+              </DropdownButton>
+          <FormControl
+                  placeholder="Recipient's username"
+                  aria-label="Amount (to the nearest dollar)"/>
+              <InputGroup.Text>$</InputGroup.Text>
+              <input type="text" pattern="[0-9]*" placeholder = "0.00"/>
+          </InputGroup.Prepend>
+      </InputGroup>
+      <Button variant="primary">Submit</Button>{' '}
+
+</div>
                     </Card>
                 </Col>
             </Row>
