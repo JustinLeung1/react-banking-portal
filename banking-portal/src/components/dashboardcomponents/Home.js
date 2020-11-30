9911e75d16bb8f88a46b41e3457aa415e275d7d7
Justin Leung <justinleung1999@hotmail.com>
2020-11-29 20:22:29 -0800

    Attempted to add account info

--- banking-portal/src/components/dashboardcomponents/Home.js
+++ banking-portal/src/components/dashboardcomponents/Home.js
@@ -1,9 +1,24 @@
-import React from 'react'
-import { Card, ListGroup } from 'react-bootstrap'
+import React, { Component } from 'react'
+import { Card, ListGroup, Accordion } from 'react-bootstrap'
 import "../../styles/Dashboard.css"
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types';
+class Home extends Component {
+    componentWillReceiveProps(nextProps) {
+        this.setState({props:nextProps})
+        // if (nextProps.UI.errors) {
+        //   this.setState({ errors: nextProps.UI.errors });
+        // }
+      }
 
+    render(){
+        const {
+            user: {
+              accounts,
+            }
+        } = this.props;
 
-function Home () {
+    console.log(accounts)
     return (
         //Home component with 3 cards
         //One header and two content cards
@@ -18,11 +33,23 @@ function Home () {
           <Card className="Tile">
             <Card.Body>
                 <h3>Bank Accounts</h3>
-                <ListGroup>
-                    <ListGroup.Item>Checking Account</ListGroup.Item>
-                    <ListGroup.Item>Savings Account</ListGroup.Item>
-                    <ListGroup.Item>Money Market Account</ListGroup.Item>
-                </ListGroup>
+                    <Card>
+                    <Card.Header>Checking Accounts</Card.Header>
+                    {
+                        accounts.map((account,index)=>{
+                            return (
+                                <Card.Body>
+                                    <Card.Title>{account.AccountID}</Card.Title>
+                                    <Card.Text>
+                                    ${account.AccountBalance.toFixed(2)}
+                                    </Card.Text>
+                                </Card.Body>
+                                )
+                        })
+                    
+                    }
+                    
+                    </Card>
             </Card.Body>
           </Card>
 
@@ -34,5 +61,17 @@ function Home () {
       </div>  
     );
 }
+}
 
-export default Home
+
+const mapStateToProps = (state) => ({
+    user: state.user
+  });
+
+
+
+  Home.propTypes = {
+    user: PropTypes.object.isRequired,
+  };
+
+export default connect(mapStateToProps)(Home);
