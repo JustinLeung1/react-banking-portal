b2f252168a0faf6b79f49cddbf6d617a785f3b04
JustinLeung1 <47397549+JustinLeung1@users.noreply.github.com>
2020-12-08 20:11:09 -0800

    took out api keys for backend

--- banking-portal-backend/functions/index.js
+++ banking-portal-backend/functions/index.js
@@ -8,13 +8,13 @@ const cors = require('cors');
 app.use(cors());
 
 const config = { 
-    apiKey: "AIzaSyADeCtcKNOi4AeH5Nt0EEA5WGjtoyDSAMw",
-    authDomain: "bankingapp-4f093.firebaseapp.com",
-    databaseURL: "https://bankingapp-4f093.firebaseio.com",
+    apiKey: env.APIKEY,
+    authDomain: env.AUTHDOMAIN,
+    databaseURL: env.DATABASEURL,
     projectId: "bankingapp-4f093",
     storageBucket: "bankingapp-4f093.appspot.com",
-    messagingSenderId: "187835655465",
-    appId: "1:187835655465:web:77849e3e9fc961b94e8338"
+    messagingSenderId: env.MESSAGINGSENDERID,
+    appId: env.APPID
 }
 
 const firebase = require('firebase');
